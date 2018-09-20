309d2c3cf5a6f6271d37bc6d6df7774922b79ea0
kganser <kganser@gmail.com>
2018-09-19 23:01:59 -0500

    http -> https; code cleanup

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ jsml
 ====
 
 Jsml (javascript markup language) is a concise way of generating pieces of the
-DOM dynamically. See <http://jsml.kganser.com> for more information.
+DOM dynamically. See <https://jsml.kganser.com> for more information.
--- jsml.js
+++ jsml.js
@@ -1,41 +1,36 @@
-// Copyright 2014, Klaus Ganser <http://kganser.com>
+// Copyright Klaus Ganser <https://kganser.com>
 // MIT Licensed, with this copyright and permission notice
 // <http://opensource.org/licenses/MIT>
 
-var jsml = function() {
-
-  var jsml, attr = function(node, parent) {
-    Object.keys(node).forEach(function(k) {
-      if (k == 'children') return;
-      var n = node[k];
-      if (typeof parent[k] == 'undefined' || typeof n != 'object' || n == null)
-        return parent[k] = n;
-      attr(n, parent[k], true);
-    });
-    return node.children;
-  };
-  
-  return jsml = function(node, parent, clear) {
-    if (clear && parent) while (parent.firstChild) parent.removeChild(parent.firstChild);
-    switch (typeof node) {
-      case 'object': // Object, Array, or null
-        if (!node) return;
-        if (Array.isArray(node)) {
-          node.forEach(function(node) { jsml(node, parent); });
-          return parent;
-        }
-        var tag = Object.keys(node)[0],
-            elem = document.createElement(tag);
-        if (parent) parent.appendChild(elem); 
-        node = node[tag];
-        jsml(typeof node == 'object' && node && !Array.isArray(node) ? attr(node, elem) : node, elem);
-        return elem;
-      case 'function':
-        return jsml(node(parent), parent);
-      case 'string':
-      case 'number':
-        node = document.createTextNode(node);
-        return parent ? parent.appendChild(node) : node;
-    }
-  };
-}();
+function jsml(node, parent, clear) {
+  if (clear && parent) while (parent.firstChild) parent.removeChild(parent.firstChild);
+  switch (typeof node) {
+    case 'object': // Object, Array, or null
+      if (!node) return;
+      if (Array.isArray(node)) {
+        node.forEach(function(node) { jsml(node, parent); });
+        return parent;
+      }
+      var tag = Object.keys(node)[0],
+          elem = document.createElement(tag);
+      if (parent) parent.appendChild(elem);
+      node = node[tag];
+      jsml(typeof node == 'object' && node && !Array.isArray(node) ? function attr(node, parent) {
+        Object.keys(node).forEach(function(k) {
+          if (k == 'children') return;
+          var n = node[k];
+          if (typeof parent[k] == 'undefined' || typeof n != 'object' || n == null)
+            return parent[k] = n;
+          attr(n, parent[k], true);
+        });
+        return node.children;
+      }(node, elem) : node, elem);
+      return elem;
+    case 'function':
+      return jsml(node(parent), parent);
+    case 'string':
+    case 'number':
+      node = document.createTextNode(node);
+      return parent ? parent.appendChild(node) : node;
+  }
+};
